fbb88f5b6b7d119a31d291f6083e5fc9f6e67e5a
harelf3 <81562611+harelf3@users.noreply.github.com>
2021-08-15 22:27:47 +0300

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>",views.get, name = "get"),
+    path("wiki/make", views.make, name = "make"),
+    path("wiki/random",views.randoms, name ="randoms"),
+    
 ]
